7683f7a05fd29b37920f7e510d87b1274c205812
rafipopz98 <itsrealrafi@gmail.com>
2024-06-01 23:08:16 +0530

    vercel push

--- lru-cache-backend/routes/routes.go
+++ lru-cache-backend/routes/routes.go
@@ -1,14 +1,12 @@
 package routes
 
 import (
-	"lru-cache-backend/demo"
 	"lru-cache-backend/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CacheRouter(incomingRoute *gin.Engine) {
-	incomingRoute.GET("/", demo.HelloWorld())
 	incomingRoute.GET("/get", services.GetCache())
 	incomingRoute.POST("/add", services.PostCache())
 	incomingRoute.GET("/all", services.GetAllCache())
